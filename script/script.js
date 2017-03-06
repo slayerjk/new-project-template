c2e0c5650c16d5e499fca270d3587eaff2e05679
slayerjk <slayer.jk@mail.ru>
2017-03-06 15:09:39 +0600

    Add check for event.preventDefault() for IE9.

--- script/script.js
+++ script/script.js
@@ -3,5 +3,15 @@
 $(document).ready(function () {
   'use strict';
 
+/*event.preventDefault() gor IE9(event.returnValue = false)*/
+  $.fn.eventPreventDefaultSafe = function () {
+    if (event.preventDefault) {
+      event.preventDefault();
+    } else {
+      event.returnValue = false;
+    }
+  };
+/*-------------------------------------------------------*/
+
 
 });
