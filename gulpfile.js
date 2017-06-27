4c07a5e5eb210535f74893a41dae206a20426a26
slayerjk <slayer.jk@mail.ru>
2017-06-27 17:40:02 +0600

    Different fixes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
-var gulp        = require('gulp'), // Подключаем Gulp
-    sass        = require('gulp-sass'), //Подключаем Sass пакет,
-    concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-    plumber     = require("gulp-plumber"),
-    uglify      = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
-    cssnano     = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-    rename      = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
-    del         = require('del'), // Подключаем библиотеку для удаления файлов и папок
-    cache       = require('gulp-cache'), // Подключаем библиотеку кеширования
-    autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
-    server = require("browser-sync").create();//browser autorefresh
+"use strict";
 
-gulp.task('sass', function(){ // Создаем таск Sass
+var gulp         = require('gulp'), // Подключаем Gulp
+    autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
+    cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
+    concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    imagemin     = require('gulp-imagemin'), // Сжатие картинок
+    minifycss    = require('gulp-csso'), // Подключаем пакет для минификации CSS
+    mqpacker     = require('css-mqpacker'), // Оптимизирует media-выражения
+    plumber      = require('gulp-plumber'), // Не позволяет звершать работу gulp при ошибке
+    rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+    run          = require('run-sequence'), // Для задания очереди исполнения тасков
+    sass         = require('gulp-sass'), //Подключаем Sass пакет,
+    server       = require('browser-sync').create(),//browser autorefresh
+    svgstore     = require('gulp-svgstore'), // Создание svg слайдов
+    svgmin       = require('gulp-svgmin'), // Минификация svg слайдов
+    uglify       = require('gulp-uglifyjs'); // Подключаем gulp-uglifyjs (для сжатия JS)
+
+gulp.task('sass', function() { // Создаем таск Sass
     return gulp.src('app/sass/style.scss') // Берем источник
         .pipe(plumber())
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
@@ -19,14 +26,25 @@ gulp.task('sass', function(){ // Создаем таск Sass
         .pipe(server.stream());
 });
 
+gulp.task('html-update', function() {
+  return gulp.src('app/*.html') // Берем источник
+    .pipe(server.stream());
+});
+
+gulp.task('script-update', function() {
+  return gulp.src('app/js/script.js') // Берем источник
+    .pipe(server.stream());
+});
+
 gulp.task('server', ['sass'], function() {
   server.init({
-    server: ".",
+    server: '.',
     notify: false,
     open: true,
     cors: true,
     ui: false
-  });
+  })
+});
 
 gulp.task('script-min', function() {
     return gulp.src('app/js/script.js') //script.js in app/js
@@ -37,41 +55,61 @@ gulp.task('script-min', function() {
 
 gulp.task('css-min', function() {
     return gulp.src('app/css/style.css') //style.css in app/css
-        .pipe(cssnano()) // Сжимаем
+        .pipe(mqpacker({sort: true})) //Оптимизация(сортировка) медиа-выражений
+        .pipe(minifycss()) // Сжимаем
         .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
         .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
 });
 
+gulp.task('symbols', function() {
+  return gulp.src('app/img/icons/*.svg')
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('symbols.svg'))
+    .pipe(gulp.dest('build/img'));
+});
+
 gulp.task('watch', ['sass'], function() {
     gulp.watch('app/sass/**/*.+(scss|sass)', ['sass']); // Наблюдение за sass файлами
+    gulp.watch('app/*.html', ['html-update']);
+    gulp.watch('app/js/script.js', ['script-update']);
     // Наблюдение за другими типами файлов
+    server.init({
+      server: "app/"
+    });
 });
 
 gulp.task('clean', function() {
-    return del.sync('prod'); // Удаляем папку dist перед сборкой
+    return del.sync('build'); // Удаляем папку сборки проекта build перед следующей сборкой
 });
 
-gulp.task('build', ['clean', 'sass', 'script-min', 'css-min'], function() {
+gulp.task('build', ['clean', 'sass', 'script-min', 'css-min', 'symbols'], function() {
     var buildCss = gulp.src('app/css/style.min.css')
-    .pipe(gulp.dest('prod/css'))
+    .pipe(gulp.dest('build/css'))
 
     var buildCss = gulp.src('app/libs/*.css')
-    .pipe(gulp.dest('prod/libs'))
+    .pipe(gulp.dest('build/libs'))
 
-    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
-    .pipe(gulp.dest('prod/fonts'))
+    var buildFonts = gulp.src('app/fonts/**/*.{woff,woff2}*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('build/fonts'))
 
-    var buildImg = gulp.src('app/img/**/*') // Переносим картинки в продакшен
-    .pipe(gulp.dest('prod/img'))
+    var buildImg = gulp.src("app/img/**/*.{png,jpg,gif}") // Переносим картинки в продакшен
+    .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),//1-max.; 3-safe; 10-no compress.
+    //imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
 
-    var buildJs = gulp.src('app/libs/*.js') // Переносим скрипты в продакшен
-    .pipe(gulp.dest('prod/libs'))
+    var buildLibs = gulp.src('app/libs/*.js') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('build/libs'))
 
     var buildJs = gulp.src('app/js/script.min.js') // Переносим основной скрипт проекта в продакшен
-    .pipe(gulp.dest('prod/js'))
+    .pipe(gulp.dest('build/js'))
 
     var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
-    .pipe(gulp.dest('prod'));
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('clear', function () {
